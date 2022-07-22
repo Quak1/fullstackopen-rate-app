e7cd8b5494623495de6b569a576919241ba5e54a
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-22 14:21:43 -0700

    Basic routing

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from "react-native";
+import { Route, Routes, Navigate } from "react-router-native";
 
 import theme from "../theme";
 import RepositoryList from "./RepositoryList";
@@ -16,7 +17,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
