0a6697216509eaef1aae69a5c36948dd573a33de
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-22 17:05:11 -0700

    Exercise 10.9: validating the sign in form

--- src/components/Form/TextInput.jsx
+++ src/components/Form/TextInput.jsx
@@ -3,17 +3,21 @@ import { TextInput as NativeTextInput, StyleSheet } from "react-native";
 import theme from "../../theme";
 
 const styles = StyleSheet.create({
-  borderColor: theme.colors.textSecondary,
-  borderWidth: 1,
-  borderRadius: 5,
-  fontSize: theme.fontSizes.subheading,
-  paddingVertical: 5,
-  paddingHorizontal: 10,
-  marginBottom: 10,
+  input: {
+    borderColor: theme.colors.textSecondary,
+    borderWidth: 1,
+    borderRadius: 5,
+    fontSize: theme.fontSizes.subheading,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  onError: {
+    borderColor: theme.colors.error,
+  },
 });
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [styles, style];
+  const textInputStyle = [styles.input, style, error && styles.onError];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,19 @@
 import { View, StyleSheet } from "react-native";
 import { Formik } from "formik";
+import * as yup from "yup";
 
 import SignInForm from "./Form/SignInForm";
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Username is required"),
+  password: yup
+    .string()
+    .min(5, "Username must be at least 5 characters long")
+    .required("Password is required"),
+});
 
 const initialValues = {
   username: "",
@@ -18,7 +30,11 @@ const SignIn = () => {
 
   return (
     <View style={styles}>
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
         {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
       </Formik>
     </View>
