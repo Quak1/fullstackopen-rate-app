79a84c255efcd6d6fbaadd3ecdb6644a32b06928
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-23 17:42:19 -0700

    Exercise 10.11: fetching repositories with Apollo Client

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,26 +1,26 @@
 import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
 
+import { GET_REPOSITORIES } from "../graphql/queries";
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
-
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    const response = await fetch(
-      "http://192.168.206.158:5000/api/repositories"
-    );
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
+  const { loading, error, data, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
 
   useEffect(() => {
-    fetchRepositories();
-  }, []);
+    if (!loading) {
+      setRepositories(data.repositories);
+    }
+  }, [loading]);
 
-  return { repositories, loading, refetch: fetchRepositories };
+  if (error) console.log("useRepositories error:", error);
+
+  return {
+    repositories,
+    loading,
+    refetch,
+  };
 };
 
 export default useRepositories;
